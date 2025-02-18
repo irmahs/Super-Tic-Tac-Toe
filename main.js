c36baf31c0a0c80c9768458f110e494a3a842ace
yumamri <irma.houversing@gmail.com>
2025-02-18 22:09:20 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const electronReload = require('electron-reload');
+
+electronReload(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+});
+
+let mainWindow;
+
+app.whenReady().then(() => {
+    mainWindow = new BrowserWindow({
+    width: 600,
+    height: 600,
+    webPreferences: {
+        nodeIntegration: true
+    }
+});
+
+    mainWindow.loadFile('index.html');
+    mainWindow.webContents.openDevTools();
+});
